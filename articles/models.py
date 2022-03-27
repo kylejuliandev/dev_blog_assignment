2cb6b249214f6ff70fe73ffc207ef99230ff360e
kylejuliandev <38759683+kylejuliandev@users.noreply.github.com>
2022-03-27 12:32:08 +0100

    Fix issue with articles_article foreign key column name

--- articles/models.py
+++ articles/models.py
@@ -10,4 +10,4 @@ class Article(models.Model):
     created_on = models.DateTimeField('article written on')
     updated_on = models.DateTimeField('article updated on')
     content = models.CharField('content', max_length=4000)
-    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
